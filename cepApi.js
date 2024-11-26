ea2e2d4cdcedf0cd18c45946818f479cfb62f398
Islânia Gomes <163878926+IslaniaGomes@users.noreply.github.com>
2024-11-26 14:13:21 -0300

    up

--- cepApi.js
+++ cepApi.js
@@ -11,4 +11,31 @@ const buscarCep =  async( ) => {
     preencheForm(endereco);
 }
 
-document.getElementById('cep').addEventListener('focusout', buscarCep)
+
+document.getElementById('cep').addEventListener('keypress', (e) => {
+    if (/[a-zA-Z]/.test(e.key)) {
+        e.preventDefault();
+    } 
+});
+
+const inputCep = document.getElementById('cep');
+
+inputCep.addEventListener('input', () => {
+    let cep = inputCep.value.replace(/\D/g, ''); 
+    let formataCep = '';
+
+    if (cep.length > 5) {
+        formataCep = cep.slice(0, 5) + '-' + cep.slice(5, 8); 
+    } else {
+        formataCep = cep; 
+    }
+
+    inputCep.value = formataCep; 
+
+});
+
+document.getElementById('cep').addEventListener('keyup', () => {
+    const cep = document.getElementById('cep').value.replace('-', ''); 
+    if (cep.length === 8) buscarCep();
+});
+
--- main.js
+++ main.js
@@ -1,24 +1,48 @@
 const form = document.querySelector('#form');
 const inputs = document.querySelectorAll('#form input');
+const textareas = document.querySelectorAll('textarea');
 
 form.addEventListener('submit', (event) => {
+
     let campoValid = true;
+    let campPreenchidos = true; 
 
    
     inputs.forEach(input => {
-
-        if (input.disabled) return; 
+        if (input.disabled) return;
 
         if (input.value.trim() === '') {
-            campoValid = false;
+            campPreenchidos = false; 
         }
-        
     });
 
-    if (!campoValid) {
+    
+    textareas.forEach(textarea => {
+        if (textarea.disabled) return;
+
+        if (textarea.value.trim() === '') { 
+            campPreenchidos = false;
+        }
+    });
+
+    
+    const cepInput = document.querySelector('#cep'); 
+
+    if (cepInput) {
+        const cep = cepInput.value.replace(/\D/g, ''); 
+        if (cep.length !== 8) {
+            campoValid = false; 
+        }
+    }
+
+    if (!campPreenchidos) {
         event.preventDefault();
-        alert('Preencha todos os campos!');
+        alert('Por favor, preencha todos os campos!');
+    } else if (!campoValid) {
+        event.preventDefault();
+        alert('O CEP deve conter 8 dígitos!');
     } else {
         alert('Enviado com Sucesso!');
+        form.reset();
     }
 });
